0bbcf27bd2842cfe8903aba39724be5f85b842eb
utkirbek <utkirbek@mail.ru>
2024-03-13 16:29:23 +0500

    HttpClient & Gson

--- src/main/java/gson/Currency.java
+++ src/main/java/gson/Currency.java
@@ -1,14 +1,21 @@
 package gson;
 
 
+import com.google.gson.annotations.SerializedName;
 import lombok.Data;
+
+import java.util.Date;
 
 @Data
 public class Currency {
     private Integer id;
     private String Code;
     private String CcyNm_UZ;
-    private String Rate;
+    private Double Rate;
+    private String Ccy;
+
+    @SerializedName("Date")
+    private Date sana;
 
 
 }
--- src/main/java/gson/GsonTest.java
+++ src/main/java/gson/GsonTest.java
@@ -1,20 +1,63 @@
 package gson;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import lombok.User;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class GsonTest {
     public static void main(String[] args) throws Exception {
 
+       String json=getJsonFromApi();
 
+        Gson gson = new GsonBuilder()
+                .setDateFormat("dd.MM.yyyy")
+                .create();
+
+        Type type = TypeToken.getParameterized(ArrayList.class, Currency.class).getType();
+
+        List<Currency> currencies = gson.fromJson(json, type);
+
+        Map<String, Double> rates=new HashMap<>();
+
+        for (Currency cur : currencies) {
+            rates.put(cur.getCcy(),cur.getRate());
+        }
+
+
+    }
+
+    private static String getJsonFromApi() {
+        try {
+            String url="https://cbu.uz/uz/arkhiv-kursov-valyut/json/";
+
+            HttpClient client= HttpClient.newBuilder().build();
+
+            HttpRequest request= HttpRequest.newBuilder()
+                    .uri(new URI(url))
+                    .GET()
+                    .build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            return response.body();
+        } catch (URISyntaxException | IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private static void withoutTypeToken() throws IOException {
